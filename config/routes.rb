85eadd66a7b750675e0284ba17c731fd52954977
Mimic <Abanoub.aziz92@gmail.com>
2016-10-18 16:56:03 +0200

    Creats controller site and sets it as root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
+  root 'site#index'
+
   # API routes
   namespace :api, defaults: { format: :json } do
     api_version(:module => "v1", :path => {:value => "v1"}, :default => true) do
     end
   end
-
-  # root 'welcome#home'
   
 end
